7711c38bddb5902f0538e44af1b897ff9114accc
Elian Salomon <floreschavezeliansalomon@gmail.com>
2026-05-29 00:47:34 -0600

    feat: initial commit for ms-gateway structure

---
+++ src/test/java/gateway/salud/elian/MsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package gateway.salud.elian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
